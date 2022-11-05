6484ffe4eb12128e975e6a7e16d80a500963ac59
MODARA3A <sohaibibrahim14@gmail.com>
2022-11-05 19:09:28 +0200

    new commit

--- assignment1.cpp
+++ assignment1.cpp
@@ -7,11 +7,12 @@ using namespace std;
 int main()
 {
 
-    int hours, days;
+    int hours, days, nHours;
     cout<<"Input the number of hours: ";
-    cin>>hours>>days;
+    cin>>hours;
     days=hours/24;
-
-
+    nHours=hours-(days*24);
+    cout<<"Number of days in the given hours: "<<days<<"/n"<<"Number of hours left: "<<nHours;
+    
 
 }
