23b750cbce85603e3b165ae18287d9b0c9af5a07
MODARA3A <sohaibibrahim14@gmail.com>
2022-11-28 16:09:32 +0200

    question 2 loop hw

---
+++ loop 2 .cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main () {
+    int x , y = 0, bigger = 0, smaller = 0, maximum = 0 , minimum = 0, sum=0, average;
+    for(int i=0; i<10; i++){
+        cout<<"Please enter a number: "<<endl;
+        cin>>x;
+        if(y>=x){
+            bigger = y;
+            smaller = x;
+        } else {
+            bigger = x;
+            smaller = y;
+        }
+        if(bigger>=maximum){
+            maximum = bigger;
+        } else if(smaller<=minimum){
+            minimum = smaller;
+        }
+        sum+=x;
+        y=x;
+
+    }
+
+    average = sum/10;
+
+    cout<<"The maximum number from the 10 given numbers is: "<<maximum<<endl;
+    cout<<"The minimum number from the 10 given numbers is: "<<minimum<<endl;
+    cout<<"The average number from the 10 given numbers is: "<<average<<endl;
+
+    return 0;
+}
