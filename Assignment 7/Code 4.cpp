b48c28109ad5eaf710e862a9ce245cbd77ff1012
MODARA3A <sohaibibrahim14@gmail.com>
2022-12-03 22:57:25 +0200

    Last commit Assignment 7

---
+++ Assignment 7/Code 4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main () {
+    for(int i=1; i<=12; i++){
+        for(int j=1; j<=12; j++){
+            cout<<i*j<<"\t";
+            if (j==12)
+            {
+                cout<<endl;
+            }
+            
+        }
+    }  
+    return 0;
+}
