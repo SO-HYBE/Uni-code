9418df7d97b49483d91ab8e451df19460b07261d
MODARA3A <sohaibibrahim14@gmail.com>
2022-12-03 22:05:52 +0200

    Assignment 7 p2

--- Assignment 7/Code 3.cpp
+++ Assignment 7/Code 3.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 int main () {
+    int x = 1;
     for(int i=0; i<=5; i++){
         for(int j=1; j<i; j++){
-            cout<<j<<" ";
+            cout<<x<<" ";
+            x++;
         }
         cout<<endl;
     }  
