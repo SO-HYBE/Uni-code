2df072b8c6f7b4ca8089fa0cff26318b3cc34612
Sohybe <sohaibibrahim14@gmail.com>
2023-06-04 16:17:10 +0300

    Binary Search

--- Second Term/Trial.cpp
+++ Second Term/Trial.cpp
@@ -4,104 +4,31 @@
 #include<cstdlib>
 using namespace std;
 
-class Sorting{
-    private:
-    int size;
-    int* array;
-    public:
+int arr[10] = {-10, -3, 7, 12, 13, 18 , 20 , 22 , 24 ,25};
 
-    Sorting(){
-        size = 100;
-        array = new int [size];
-    }
-
-    Sorting(int n){
-        size = n;
-        array = new int [size];
-    }
-
-    void read() {
-        for(int i=0; i<size; i++){
-            array[i]=rand()%100;
+int binary (int low, int high, int key){
+    while(low<=high){
+        int mid = (low + high)/2;
+        if(arr[mid] == key){
+            return mid;
+        } else if(arr[mid]>key){
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
+    }
+    return -1;
+}
+
+int main () {
+    int key;
+    cout<<"Enter the key you want to search inside the array: ";
+    cin>>key;
+    int result = binary(0,9,key);
+    if(result == -1){
+        cout<<"Error: The key was not found inside the array!";
+    } else {
+        cout<<"The key was found inside the array!!!";
     }
     
-    void print() {
-        for(int i=0; i<size; i++){
-            cout<< array[i]<<"\t";
-        }
-    }
-
-    void insertion(){
-        int j , temp;
-        for(int i = 1; i<=size-1; i++){
-            j=i;
-            while(j>0 && array[j-1]>array[j]){
-                temp = array[j];
-                array[j]= array[j-1];
-                array[j-1] = temp;
-                j--;
-            }
-
-        }
-    }
-
-    void bubble (){
-        int max;
-        for(int i=size-1; i>0;i--){
-            max= array[0];
-            for(int j = 1; j<=i;j++){
-                if(array[j]<max){
-                    array[j-1]= array[j];
-                    array[j]=max;
-                } else {
-                    max = array[j];
-                }
-            }
-        }
-    }
-
-    void merge(int first, int mid, int last){
-        int i1=first, i2=mid+1, k=first;
-        int * arrayout = new int [size];
-        while(i1<=mid && i2<=last){
-            if(array[i1]<=array[i2]){
-                arrayout[k] = array[i1];
-                i1++;k++;
-            } else {
-                arrayout[k] = array[i2];
-                i2++;k++;
-            }
-        }
-        while(i1<=mid){
-            arrayout[k]=array[i1]; i1++;k++;
-        }
-        while(i2<=last){
-            arrayout[k]=array[i2]; i2++;k++;
-        }
-        for(int i =first; i<=last;i++){
-            array[i] = arrayout[i];
-        }
-    }
-
-    void merge_sorting(int first, int last){
-        if(first<last){
-            int midpoint = (first+last)/2;
-            merge_sorting(first, midpoint);
-            merge_sorting(midpoint+1, last);
-            merge(first, midpoint, last);
-        }
-    }
-
-};
-
-int main (){
-    Sorting obj(10), obj1;
-    obj.read();
-    cout<<"\ncontent of array before bubble sort\n";
-    obj.print();
-    obj.merge_sorting(0,9);
-    cout<<"\ncontent of array after sort\n";
-    obj.print();
-
 }
