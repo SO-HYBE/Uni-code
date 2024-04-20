4663ab47aa271b3981a3b09310f3bd4cf0c13978
Sohybe <sohaibibrahim14@gmail.com>
2024-04-20 21:21:17 +0200

    Assignment 7

---
+++ OOP/OOPAssignment7/src/main/java/com/mycompany/oopassignment7/OOPAssignment7.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oopassignment7;
+
+/**
+ *
+ * @author sohai
+ */
+public class OOPAssignment7 {
+    public static void main(String[] args) {
+        // code
+    }
+}
